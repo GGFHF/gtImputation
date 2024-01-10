a2da36a3878a1a70db54df42a9cd9563da8798e8
Fernando Mora Márquez <fernandomoramarquez@gmail.com>
2024-01-10 11:34:22 +0100

    Mod. in v0.16

---
+++ Package/script-templates/test-imputation-starter.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#-------------------------------------------------------------------------------
+~/Documents/test-mr-QS-B/test-imputation.sh &>~/Documents/test-mr-QS-B/test-imputation-log.txt
